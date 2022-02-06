9c56666a19940e5d424e7ff19f5a2d81706bd41d
Markus Pfeiffer <markus.pfeiffer@codewild.de>
2022-02-06 10:50:22 +0100

    EclipseLink 3.0.2
    Test of EqualsVerifier 3.9 with EclipseLink 3.0.2

--- src/main/java/com/example/Student.java
+++ src/main/java/com/example/Student.java
@@ -1,16 +1,16 @@
 package com.example;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderColumn;
+import jakarta.persistence.Table;
 import org.eclipse.persistence.annotations.UuidGenerator;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderColumn;
-import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
